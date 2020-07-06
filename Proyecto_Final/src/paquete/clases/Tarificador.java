0ab47ac2cb6f63fd2584fea3d52829dbedf27420
Juan Diego Rojas <Juan Diego Rojas@LAPTOP-2RPUJN7V>
2020-07-05 23:36:02 -0500

    tarificador

--- Proyecto_Final/src/paquete/clases/Tarificador.java
+++ Proyecto_Final/src/paquete/clases/Tarificador.java
@@ -12,7 +12,7 @@ import paquete.grafo.Grafo;
  */
 public class Tarificador {
 	private String origen, destino;
-	private double precio_total;
+	
 	private int distancia;
         
 	private final double costo = 3.5;
@@ -21,8 +21,14 @@ public class Tarificador {
         this.origen = origen;
         this.destino = destino;
         
-        this.precio_total = calcularPrecio(this.origen, this.destino);
+        
     }
+
+    Tarificador() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
 
     public String getOrigen() {
         return origen;
@@ -48,9 +54,13 @@ public class Tarificador {
         this.distancia = distancia;
     }
 
-    public double calcularPrecio(String origen, String destino){
+    public double calcularPrecio(){
         Grafo t = new Grafo(); 
-        double precio_tot = costo * t.caminoOptimo(origen, destino);
+        double precio_tot = costo * t.caminoOptimo(this.origen, this.destino);
         return precio_tot;
+    }
+    public static void main(String[] args) {
+        Tarificador p = new Tarificador("San Miguel","Surco");
+        System.out.println(p.calcularPrecio());
     }
 }
