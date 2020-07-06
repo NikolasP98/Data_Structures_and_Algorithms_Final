ab604414393f9f3e499ff06117668f7059f879e1
Juan Diego Rojas <Juan Diego Rojas@LAPTOP-2RPUJN7V>
2020-07-06 02:58:41 -0500

    lista comprobante

--- Proyecto_Final/src/paquete/clases/Lista_Comprobantes.java
+++ Proyecto_Final/src/paquete/clases/Lista_Comprobantes.java
@@ -7,20 +7,39 @@ package paquete.clases;
 
 import edu.ulima.eda.pilas.Pila;
 import paquete.clases.*;
-import edu.ulima.eda.pilas.*;
+import edu.ulima.eda.listas.*;
 /**
  *
  * @author Juan Diego Rojas
  */
-public class Lista_Comprobantes extends Pila {
-    
-    
+public class Lista_Comprobantes extends ListaEnlazada {
+    ListaEnlazada lista = new ListaEnlazada();
+    private double totalEncomiendas, totalGiros, totalGeneral;
+
+    public void Totales(){
+        while (!Lista_Comprobantes.estaVacia()){
+            Comprobante x = pila.desapilar();
+
+            if(x.getTipoTexto == "Giro"){
+                totalGiros += x.getmonto;
+                totalGeneral += x.getmonto;
+
+            }else{
+                totalEncomiendas += x.getmonto;
+                totalGeneral += x.getmonto;
+            }
+
+            lista.insertarAlFinal(x);
+        }
+    }
+
+
 
 public static void main(String[] args) {
-        
+
         Pila z = new Pila();
         Comprobante t = new Comprobante (1,"CiudadOrigen","CiudadDestino",150);
         z.desapilar();
-        
+
     }
 }
