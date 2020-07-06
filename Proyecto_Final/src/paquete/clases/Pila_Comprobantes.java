be9c0eeccc70432b07431ac54c6b1a97baff10ff
NikolasP98 <nikolas.pinon98@gmail.com>
2020-07-06 02:43:31 -0500

    clase pila agregada

---
+++ Proyecto_Final/src/paquete/clases/Pila_Comprobantes.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete.clases;
+
+import edu.ulima.eda.pilas.Pila;
+
+/**
+ *
+ * @author Nikolas
+ */
+public class Pila_Comprobantes extends Pila{
+    
+    
+    
+    
+    
+}
