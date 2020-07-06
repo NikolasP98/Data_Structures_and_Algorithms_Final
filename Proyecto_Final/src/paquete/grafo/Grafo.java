da65b1e3e68af2783ca413287fa2373111e0e643
NikolasP98 <nikolas.pinon98@gmail.com>
2020-07-05 21:19:00 -0500

    algoritmo camino mas corto

--- Proyecto_Final/src/paquete/grafo/Grafo.java
+++ Proyecto_Final/src/paquete/grafo/Grafo.java
@@ -16,98 +16,84 @@ package paquete.grafo;
  */
 
 public class Grafo {
-    
-    static final int cant_Vertices = 15;
 
-    int minDistancia(int dist[], Boolean camino_valido[]) 
-    { 
+    int grafo[][] = new int[][] {
+        /*SAN MIGUEL*/  { 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        /*MAGDALENA*/   { 2, 0, 2, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        /*SAN ISIDRO*/  { 0, 2, 0, 1, 0, 0, 2, 0, 2, 3, 0, 0, 0, 0, 0},
+        /*MIRAFLORES*/  { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0},
+        /*PUEBLO LIBRE*/{ 1, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        /*JESUS MARIA*/ { 0, 1, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+        /*SURQUILLO*/   { 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
+        /*BARRANCO*/    { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0},
+        /*LINCE*/       { 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        /*SAN BORJA*/   { 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 0, 0},
+        /*CHORRILLOS*/  { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+        /*SURCO*/       { 0, 0, 0, 4, 0, 0, 3, 2, 0, 2, 1, 0, 0, 3, 4},
+        /*ATE*/         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 2, 0},
+        /*LA MOLINA*/   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 3},
+        /*SJM*/         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 3, 0},
+        };
+
+    static final int cant_Vertices = 15;
+    String distritos[] = new String[] {
+        "SAN MIGUEL", "MAGDALENA", "SAN ISIDRO", "MIRAFLORES", "PUEBLO LIBRE", "JESUS MARIA", "SURQUILLO", "BARRANCO", "LINCE", "SAN BORJA", "CHORRILLOS", "SURCO", "ATE", "LA MOLINA", "SJM"
+    };
+    int minDistancia(int dist[], Boolean camino_procesado[]) { 
         // int min, al final de ejecutar el algoritmo, tiene que contener la distancia minima para llegar de un punto a otro
         int min = Integer.MAX_VALUE;
         int indice_min = -1; 
   
         for (int j = 0; j < cant_Vertices; j++) {
-            if (camino_valido[j] == false && dist[j] <= min) { 
+            if (camino_procesado[j] == false && dist[j] <= min) { 
                 min = dist[j]; 
                 indice_min = j; 
             }
         }
         return indice_min; 
-    } 
-  
-    // A utility function to print the constructed distance array 
-    void printSolution(int dist[]) 
-    { 
-        System.out.println("Vertex \t\t Distance from Source"); 
-        for (int i = 0; i < cant_Vertices; i++) 
-            System.out.println(i + " \t\t " + dist[i]); 
-    } 
+    }
   
     // Aplicar algoritmo a grafo de adyacencia
-    void caminoOptimo(int grafo[][], int src) 
-    { 
+    public int caminoOptimo(String src, String end) { 
         int dist[] = new int[cant_Vertices];
   
-        // camino_valido[i] will true if vertex i is included in shortest 
+        // camino_procesado[i] will true if vertex i is included in shortest 
         // path tree or shortest distance from src to i is finalized 
-        Boolean camino_valido[] = new Boolean[cant_Vertices]; 
+        Boolean camino_procesado[] = new Boolean[cant_Vertices]; 
   
-        // Initialize all distances as INFINITE and stpSet[] as false 
+        // Inicializar arreglo de distancia[] y arreglo booleano de camino_procesado[]
         for (int i = 0; i < cant_Vertices; i++) { 
             dist[i] = Integer.MAX_VALUE; 
-            camino_valido[i] = false; 
+            camino_procesado[i] = false; 
         } 
   
-        // Distance of source vertex from itself is always 0 
-        dist[src] = 0; 
+        // Parsear el texto ingresado por el usuario a mayusculas para evitar errores
+        int indice_origen = java.util.Arrays.asList(distritos).indexOf(src.toUpperCase());
+        int indice_fin = java.util.Arrays.asList(distritos).indexOf(end.toUpperCase());
+
+        dist[indice_origen] = 0; 
   
-        // Find shortest path for all cant_Verticess 
-        for (int count = 0; count < cant_Vertices - 1; count++) { 
-            // Pick the minimum distance vertex from the set of cant_Verticess 
-            // not yet processed. u is always equal to src in first 
-            // iteration. 
-            int u = minDistancia(dist, camino_valido); 
+        // buscar el camino mas corto del origen a todos los demas destinos
+        for (int contador = 0; contador < cant_Vertices - 1; contador++) { 
+            int u = minDistancia(dist, camino_procesado); 
   
-            // Mark the picked vertex as processed 
-            camino_valido[u] = true; 
-  
-            // Update dist value of the adjacent cant_Verticess of the 
-            // picked vertex. 
+            // Indica si el vertice ha sido procesado en el paso anterior
+            camino_procesado[u] = true; 
             for (int v = 0; v < cant_Vertices; v++) 
   
-                // Update dist[v] only if is not in camino_valido, there is an 
-                // edge from u to v, and total weight of path from src to 
-                // v through u is smaller than current value of dist[v] 
-                if (!camino_valido[v] && grafo[u][v] != 0 && dist[u] != Integer.MAX_VALUE && dist[u] + grafo[u][v] < dist[v]) 
+                if (!camino_procesado[v] && grafo[u][v] != 0 && dist[u] != Integer.MAX_VALUE && dist[u] + grafo[u][v] < dist[v]) 
                     dist[v] = dist[u] + grafo[u][v]; 
         } 
   
-        // print the constructed distance array 
-        printSolution(dist); 
+        // devolver la distancia minima del origen al destino
+        System.out.println(dist[indice_fin]);
+        return dist[indice_fin];
     } 
   
-    // Driver method 
     public static void main(String[] args) 
-    { 
-        /* Let us create the example grafo discussed above */
-        int grafo[][] = new int[][] {
-            /*SAN MIGUEL*/  { 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            /*MAGDALENA*/   { 2, 0, 2, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            /*SAN ISIDRO*/  { 0, 2, 0, 1, 0, 0, 2, 0, 2, 3, 0, 0, 0, 0, 0},
-            /*MIRAFLORES*/  { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 4, 0, 0, 0},
-            /*PUEBLO LIBRE*/{ 1, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            /*JESUS MARIA*/ { 0, 1, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-            /*SURQUILLO*/   { 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0},
-            /*BARRANCO*/    { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0},
-            /*LINCE*/       { 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            /*SAN BORJA*/   { 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 0, 0},
-            /*CHORRILLOS*/  { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
-            /*SURCO*/       { 0, 0, 0, 4, 0, 0, 3, 2, 0, 2, 1, 0, 0, 3, 4},
-            /*ATE*/         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 2, 0},
-            /*LA MOLINA*/   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 3},
-            /*SJM*/         { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 3, 0},
-                                    }; 
+    {
         Grafo t = new Grafo(); 
-        t.caminoOptimo(grafo, 0); 
+        t.caminoOptimo("SJM", "LA MOLINA");
     } 
 } 
 
